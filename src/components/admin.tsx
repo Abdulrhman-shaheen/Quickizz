07a8ab2acc41899bf5b8a414c72addf10b9b0aa6
ali-abohendy <ali.abohendy@ejust.edu.eg>
2024-12-24 14:11:10 +0200

    HandledSubmeit for login

--- src/components/admin.tsx
+++ src/components/admin.tsx
@@ -1,35 +1,14 @@
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
+import PageTransition from "./pagetransition";
 import { handleSubmit } from "../utils/handlesubmit.ts";
 
 function Admin() {
-  const previousAnimation = localStorage.getItem("animation") || "100vh";
   return (
-    <motion.div
-      className="box-border h-screen w-screen flex flex-col justify-center items-center"
-      variants={{
-        initial: {
-          x: previousAnimation,
-        },
-        final: {
-          x: "0vh",
-        },
-      }}
-      initial="initial"
-      animate="final"
-      transition={{ duration: 0.3 }}
-      onAnimationComplete={() => {
-        localStorage.setItem(
-          "animation",
-          previousAnimation === "-100vh" ? "100vh" : "-100vh"
-        );
-        document.querySelector("body")?.classList.remove("overflow-hidden")
-      }}
-      onAnimationStart={() => {document.querySelector("body")?.classList.add("overflow-hidden")}}
-    >
+<PageTransition>
       <div className="flex flex-col justify-center items-center w-2/5 min-w-96 text-center text-2xl text-[#FFFFFF] gap-9 bg-black p-8 rounded-3xl border-[#302f2f] border">
         <h1 className="text-[#FFFFFF] text-6xl mb-6"> Welcome Lecturer </h1>
-        <form className="flex flex-col gap-3" onSubmit={(e) => handleSubmit(e, "http://localhost:5000/login")}>
+        <form className="flex flex-col gap-3 space-y-7 items-center" onSubmit={(e) => handleSubmit(e, "http://localhost:5000/login")}>
+        <div className="flex flex-col gap-3">
           <input
             className="rounded-xl border-[#302f2f] border text-white gap-2 w-60 cursor-text font-medium bg-black min-w-80 px-3 py-5 text-base"
             type="text"
@@ -40,15 +19,14 @@ function Admin() {
             type="password"
             placeholder="Password"
           />
-        </form>
+          </div>
         <div className="flex flex-col gap-2 justify-center items-center">
-          <Link
-            className="visited:text-black hover:bg-gray-200 no-underline flex justify-center rounded-lg px-3 py-5 border text-xl-1 cursor-pointer bg-white w-60 font-medium text-black"
-            to="/admininter"
+        <Link to="/admin/admininter"> <button type='submit'
+            className="visited:text-black hover:bg-gray-200 no-underline flex justify-center rounded-lg p-3 border text-xl-1 cursor-pointer bg-white w-60 font-medium text-black"
           >
             {" "}
             Login{" "}
-          </Link>
+          </button></Link>
           <p className="text-base">
             {" "}
             Don&apos;t have account?{" "}
@@ -57,8 +35,9 @@ function Admin() {
             </Link>
           </p>
         </div>
+        </form>
       </div>
-    </motion.div>
+    </PageTransition>
   );
 }
 export default Admin;
