c84761f899f0a62806494cb674619642628fae31
Abdelrhman Shaheen <abdelrhman.shaheen@ejust.edu.eg>
2024-12-24 02:41:37 +0200

    minor changes

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -17,28 +17,19 @@ function Buttons() {
       initial="initial"
       animate="final"
       onAnimationComplete={() => {
-        localStorage.setItem(
-          "animation",
-          previousAnimation === "-100vh" ? "100vh" : "-100vh"
-        );
+        localStorage.setItem("animation", previousAnimation === "-100vh" ? "100vh" : "-100vh");
       }}
     >
       <div className="flex flex-col justify-center items-center text-2xl text-[#FFFFFF] gap-20 bg-black p-20 rounded-3xl border-[#302f2f] border">
         <h1 className="text-[#FFFFFF] text-7xl mb-4">
-          Welcome to <span>QUICKIZZ</span>
+          Welcome to <span className="font-outfit">QUICKIZZ</span>
         </h1>
         <div className="flex flex-col gap-4 items-center">
-          <Link
-            className="visited:text-black flex justify-center rounded-lg px-3 py-5 border text-xl-1  bg-white w-60 font-medium text-black"
-            to="/user"
-          >
+          <Link className="visited:text-black flex justify-center rounded-lg px-3 py-5 border text-xl-1  bg-white w-60 font-medium text-black" to="/user">
             {" "}
             Login as User{" "}
           </Link>
-          <Link
-            className="visited:text-black flex justify-center rounded-lg px-3 py-5 border text-xl-1  bg-white w-60 font-medium text-black"
-            to="/admin"
-          >
+          <Link className="visited:text-black flex justify-center rounded-lg px-3 py-5 border text-xl-1  bg-white w-60 font-medium text-black" to="/admin">
             Login as Admin
           </Link>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
