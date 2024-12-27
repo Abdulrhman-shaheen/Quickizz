15bf05ec3fc067c2d143d1e5e8a94709f67a41e3
Nadine <nadinemostafasaad@gmail.com>
2024-12-27 23:48:34 +0200

    modified the dialogue component to handle different types of users

--- src/components/lecturerhome.tsx
+++ src/components/lecturerhome.tsx
@@ -1,28 +1,59 @@
 import Header from "../components/header";
+import { User } from "../types/user";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getUser } from "../utils/getUser";
+import Dialogue from "./dialogue";
 
 function LecturerHome() {
-
-    const addIcon = (<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 7.757v8.486M7.757 12h8.486M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
-                    </svg>)
-
+    const navigate = useNavigate();
+    const [user, setUser] = useState<User | null>(null);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    
+    const toggleDialog = () => {
+      setDialogOpen(!dialogOpen);
+    };
+  
+    useEffect(() => {
+      if (document.cookie == "") {
+        navigate("/");
+      }
+      const fetchData = async () => {
+        const result = await getUser(
+          `${import.meta.env.VITE_BACKEND_URL}/getuser`
+        );
+        setUser(result);
+      };
+  
+      fetchData();
+    }, []);
+  
+    const quizzes = user ? user["quizzes"] : [];
+  
     return (
-        <div>
-            {/* TO DO: Handle user data in the header*/}
-            <Header user={null} navigate={undefined} />
-
-            <div className="mt-48 text-lg flex flex-col items-center justify-center gap-6">
-
-                <button className="w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-                Past Quizzes
-                </button>
-
-                <button className="gap-4 flex flex-row items-center justify-center w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-                {addIcon} Create New Quiz
-                </button>
-                
+      <div>
+        <Header user={user} navigate={navigate} />
+        {quizzes.length > 0 ? (
+          quizzes.map((quiz) => (
+            <div key={quiz} className="flex  m-12 gap-10">
+              <div className="border min-w-96 h-96"></div>
             </div>
+          ))
+        ) : (
+          <div className="flex justify-center items-center h-96 text-2xl font-inter text-gray-200 italic">
+            No previous quizzes available
+          </div>
+        )}
+        <div className="absolute bottom-10 right-10 mr-auto text-2xl font-inter text-gray-300 italic">
+          <button onClick={() => toggleDialog()} className="bg-white hover:bg-gray-300  text-black font-bold py-2 px-4 rounded-full transform transition-transform duration-600 hover:rotate-180">
+            +
+          </button>
+          {dialogOpen && (<Dialogue toggle={toggleDialog} navigate={navigate} userType={"lecturer"}/>
+  )}
+  
         </div>
+      </div>
     );
-}
+  }
+  
 export default LecturerHome;
