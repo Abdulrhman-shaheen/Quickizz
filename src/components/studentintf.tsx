023073105758877bf89beab5fd93c8460ec059f9
ali-abohendy <ali.abohendy@ejust.edu.eg>
2024-12-27 09:20:28 +0200

    Added cookies to the student interface

--- src/components/studentintf.tsx
+++ src/components/studentintf.tsx
@@ -1,11 +1,31 @@
 import QuestionsTransition from "./questiontransition";
 import Question from "./question";
 import {fetchingData} from "../utils/fetchingData";
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import { getUser } from "../utils/getUser";
+import { useNavigate } from "react-router-dom";
+import { User } from "../types/user";
+
 
 function StudentIntf() {
 
   let [score, setScore] = useState(0);
+  const navigate = useNavigate();
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    if (document.cookie == "") {
+      navigate("/");
+    }
+    const fetchData = async () => {
+      const result = await getUser(
+        `${import.meta.env.VITE_BACKEND_URL}/getuser`
+      );
+      setUser(result);
+    };
+
+    fetchData();
+  }, []);
 
   // let tempdata = 
   //   [{_id: "As5d5dd98d",
@@ -23,27 +43,33 @@ function StudentIntf() {
   after setting the backend.
   */
   let data = fetchingData(`${import.meta.env.VITE_BACKEND_URL}/questions`);
-
   let total = data.length;
-  let login_data = fetchingData(`${import.meta.env.VITE_BACKEND_URL}/login`);
   return (
     <div>
       <header className="border-b-2 text-black text-2xl p-6">
         <div className="flex flex-row justify-between items-center">
-          {login_data.map((user) => (
-          <h1
-            className="text-white before:content-attr-letters before:inline-block before:w-10 before:h-10 before:text-center before:bg-white before:text-black before:leading-10 before:rounded-full before:align-middle before:text-base before:mr-4"
-            data-letters={user.firstname.charAt(0) + user.lastname.charAt(0)}
-          >
-            {user.firstname} {" "} {user.lastname}
-          </h1>
-          ))}
+        <h1
+          className="text-white before:content-attr-letters before:inline-block before:w-10 before:h-10 before:text-center before:bg-white before:text-black before:leading-10 before:rounded-full before:align-middle before:text-base before:mr-4"
+          data-letters={
+            user ? user.firstname.charAt(0) + user.lastname.charAt(0) : ""
+          }
+        >
+          {user ? `${user.firstname} ${user.lastname}` : ""}
+        </h1>
+          
           <p className="text-white flex items-center">
             Score: {score}/{total}
           </p>
-          <button className="bg-white text-xl flex justify-center font-semibold shadow-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1 text-black py-1 px-2 rounded-lg">
-            Sign out
-          </button>
+          <button
+          onClick={() => {
+            document.cookie =
+              "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+              navigate("/");
+          }}
+          className="bg-white text-xl flex justify-center font-semibold shadow-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-1 text-black py-1 px-2 rounded-lg"
+        >
+           Sign out
+           </button>
         </div>
       </header>
       <div className="flex flex-col mt-5 gap-2 items-center">
