b48df5c48551a6f0772e05f0ca908aa85b54ab84
Nadine <nadinemostafasaad@gmail.com>
2024-12-27 05:03:33 +0200

    Added the header in the lecturer home

--- src/components/lecturerhome.tsx
+++ src/components/lecturerhome.tsx
@@ -1,3 +1,5 @@
+import Header from "../components/header";
+
 function LecturerHome() {
 
     const addIcon = (<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
@@ -5,16 +7,21 @@ function LecturerHome() {
                     </svg>)
 
     return (
-        <div className="text-lg flex flex-col items-center justify-center gap-6 h-screen w-screen">
+        <div>
+            {/* TO DO: Handle user data in the header*/}
+            <Header user={null} navigate={undefined} />
 
-            <button className="w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            Past Quizzes
-            </button>
+            <div className="mt-48 text-lg flex flex-col items-center justify-center gap-6">
 
-            <button className="gap-4 flex flex-row items-center justify-center w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
-            {addIcon} Create New Quiz
-            </button>
-            
+                <button className="w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                Past Quizzes
+                </button>
+
+                <button className="gap-4 flex flex-row items-center justify-center w-1/6 h-24 bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+                {addIcon} Create New Quiz
+                </button>
+                
+            </div>
         </div>
     );
 }
